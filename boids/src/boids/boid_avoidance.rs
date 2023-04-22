df5af3c525a44016cfdccb6567f2c70de0d7cb15
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-04-22 18:54:47 +0300

    ♻️ Adjusts code (#1)
    * :wrench: Uses Sparse protocol
    
    * :package: Updates cargo locks
    
    * :construction_worker: Adds PR checking
    
    * :recycle: Refactors for iterators

--- boids/src/boids/boid_avoidance.rs
+++ boids/src/boids/boid_avoidance.rs
@@ -12,28 +12,30 @@ pub fn boid_avoidance(
     other_query: Query<&Transform, Without<Player>>,
     time: Res<Time>,
 ) {
-    for (mut vector, transform) in boid_query.iter_mut() {
-        if transform.translation.x.is_nan() {
-            continue;
-        }
-        let mut direction = 0.0;
-        for other_transform in other_query.iter() {
-            if transform == other_transform || other_transform.translation.x.is_nan() {
-                continue;
-            }
-            let distance = other_transform.translation.distance(transform.translation);
-            if distance < constants::BOID_VISION_DISTANCE {
-                let absolute_angle =
-                    relative_angle_between(transform.translation, other_transform.translation);
-                let relative_angle = diff_angles(absolute_angle, vector.direction);
-                if relative_angle.abs() < constants::BOID_VISION_ARC {
-                    direction -= (4.0 - relative_angle.abs() / constants::BOID_VISION_ARC)
-                        * relative_angle.signum()
-                        * 4.0
-                        * (0.75 - distance / constants::BOID_VISION_DISTANCE);
+    for (mut vector, transform) in boid_query
+        .iter_mut()
+        .filter(|(_, transform)| !transform.translation.x.is_nan())
+    {
+        let direction = other_query
+            .iter()
+            .filter(|other_transform| {
+                !other_transform.translation.x.is_nan() && transform != *other_transform
+            })
+            .fold(0.0, |mut direction, other_transform| {
+                let distance = other_transform.translation.distance(transform.translation);
+                if distance < constants::BOID_VISION_DISTANCE {
+                    let absolute_angle =
+                        relative_angle_between(transform.translation, other_transform.translation);
+                    let relative_angle = diff_angles(absolute_angle, vector.direction);
+                    if relative_angle.abs() < constants::BOID_VISION_ARC {
+                        direction -= (4.0 - relative_angle.abs() / constants::BOID_VISION_ARC)
+                            * relative_angle.signum()
+                            * 4.0
+                            * (0.75 - distance / constants::BOID_VISION_DISTANCE);
+                    }
                 }
-            }
-        }
+                direction
+            });
         vector.direction += direction * time.delta_seconds();
         vector.direction %= RADIAN_MAX;
     }
--- boids/src/main.rs
+++ boids/src/main.rs
@@ -2,11 +2,10 @@ use bevy::app::App;
 use bevy::ecs::component::Component;
 use bevy::ecs::system::Commands;
 use bevy::prelude::{
-    AssetServer, Camera2dBundle, Input, KeyCode, Plugin, Quat, Query, Res, ResMut, Resource,
-    Transform, With, Without,
+    AssetServer, Camera2dBundle, Plugin, Quat, Query, Res, Resource, Transform, With,
 };
-use bevy::sprite::SpriteBundle;
-use bevy::time::{Time, Timer, TimerMode};
+
+use bevy::time::{Time, Timer};
 use bevy::window::{PrimaryWindow, Window};
 use bevy::DefaultPlugins;
 
@@ -34,48 +33,13 @@ pub struct Vector {
 fn setup_env(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
-    asset_server: Res<AssetServer>,
+    _asset_server: Res<AssetServer>,
 ) {
     let window = window_query.get_single().unwrap();
-    commands.spawn((
-        SpriteBundle {
-            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0)
-                .with_scale([0.25, 0.25, 1.0].into()),
-            texture: asset_server.load("sprites/kenney_simple-space/PNG/Retina/ship_J.png"),
-            ..Default::default()
-        },
-        Player {},
-        Vector {
-            direction: 0.0,
-            velocity: 0.0,
-        },
-    ));
     commands.spawn(Camera2dBundle {
         transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 1000.0),
         ..Default::default()
     });
-}
-
-fn check_player(
-    time: Res<Time>,
-    mut timer: ResMut<CheckTimer>,
-    player: Query<(&Player, &Vector, &Transform)>,
-    boid_query: Query<&Transform, Without<Player>>,
-) {
-    if timer.0.tick(time.delta()).just_finished() {
-        if let Ok((player, vector, transform)) = player.get_single() {
-            println!("{:?} is alive!", player);
-            println!("{:?}", vector);
-            println!("{:?}", transform);
-            let mut count = 0;
-            for boid_transform in boid_query.iter() {
-                if boid_transform.translation.x.is_nan() {
-                    count += 1;
-                }
-            }
-            println!("{} boids are dead", count);
-        }
-    }
 }
 
 pub struct System;
@@ -83,57 +47,13 @@ pub struct System;
 impl Plugin for System {
     fn build(&self, app: &mut App) {
         app.add_plugin(boids::Boids)
-            .insert_resource(CheckTimer(Timer::from_seconds(5.0, TimerMode::Repeating)))
             .add_startup_system(setup_env)
-            .add_system(check_player)
-            .add_system(player_control)
             .add_system(vector_motion)
             .add_system(wrap_screen_edge);
     }
 }
 
-const PLAYER_ACCEL: f32 = 160.0;
-const PLAYER_DECEL: f32 = 80.0;
-const PLAYER_MAX_VELOCITY: f32 = 800.0;
 pub const RADIAN_MAX: f32 = (360.0 * std::f32::consts::PI) / 180.0;
-
-fn player_control(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut player_query: Query<&mut Vector, With<Player>>,
-    time: Res<Time>,
-) {
-    if let Ok(mut vector) = player_query.get_single_mut() {
-        let speed_adjustment = time.delta_seconds() * PLAYER_ACCEL;
-        if keyboard_input.pressed(KeyCode::Up) {
-            vector.velocity += speed_adjustment;
-        } else if keyboard_input.pressed(KeyCode::Down) {
-            vector.velocity -= speed_adjustment;
-        } else if vector.velocity > 0.0 {
-            if vector.velocity < time.delta_seconds() * PLAYER_DECEL {
-                vector.velocity = 0.0;
-            } else {
-                vector.velocity -= time.delta_seconds() * PLAYER_DECEL;
-            }
-        } else if vector.velocity < 0.0 {
-            if vector.velocity > -time.delta_seconds() * PLAYER_DECEL {
-                vector.velocity = 0.0;
-            } else {
-                vector.velocity += time.delta_seconds() * PLAYER_DECEL;
-            }
-        }
-        if vector.velocity > PLAYER_MAX_VELOCITY {
-            vector.velocity = PLAYER_MAX_VELOCITY;
-        } else if vector.velocity < -PLAYER_MAX_VELOCITY / 2.0 {
-            vector.velocity = -PLAYER_MAX_VELOCITY / 2.0;
-        }
-        if keyboard_input.pressed(KeyCode::Left) {
-            vector.direction -= time.delta_seconds() * 2.0;
-        } else if keyboard_input.pressed(KeyCode::Right) {
-            vector.direction += time.delta_seconds() * 2.0;
-        }
-        vector.direction %= RADIAN_MAX;
-    }
-}
 
 fn vector_motion(time: Res<Time>, mut transformation_vectors: Query<(&mut Transform, &Vector)>) {
     for (mut transform, vector) in transformation_vectors.iter_mut() {
